53e1de5a56daa7bff96afe1122f796e93ea9d38a
Antoine Boyer <antoine.boyer@epfl.ch>
2022-03-11 07:36:12 +0100

    modif test

--- app/src/test/java/com/github/displace/sdp2022/ExampleUnitTest.kt
+++ app/src/test/java/com/github/displace/sdp2022/ExampleUnitTest.kt
@@ -14,49 +14,6 @@ import java.lang.Error
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
-class ExampleUnitTest {
-    @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
-    }
-
-    val mod = GameVersusViewModel()
-
-    @Test
-    fun GameViewLaunch() {
-        try {
-            mod.OnSurrend(3)
-        }catch (error: Error){
-            assertEquals(false,true)
-        }
-        assertNotEquals(null,4)
-    }
-
-    @Test
-    fun GameViewOnStart(){
-        mod.handleEvent(GameEvent.OnStart(Point(6.0,6.0),listOf(3.0,3.0,3.0),3))
-    }
-
-    @Test
-    fun GameViewOnPointSelectedWin(){
-        mod.handleEvent(GameEvent.OnStart(Point(6.0,6.0),listOf(3.0,3.0,3.0),3))
-        mod.handleEvent(GameEvent.OnPointSelected(3, Point(6.0,6.0)))
-    }
-
-    @Test
-    fun GameViewOnPointSelectedLose(){
-        mod.handleEvent(GameEvent.OnStart(Point(6.0,6.0),listOf(3.0,3.0,3.0),3))
-        mod.handleEvent(GameEvent.OnPointSelected(3, Point(12.0,12.0)))
-        mod.handleEvent(GameEvent.OnPointSelected(3, Point(12.0,12.0)))
-        mod.handleEvent(GameEvent.OnPointSelected(3, Point(12.0,12.0)))
-        mod.handleEvent(GameEvent.OnPointSelected(3, Point(12.0,12.0)))
-    }
-
-    @Test
-    fun GameViewOnSurrend(){
-        mod.handleEvent(GameEvent.OnStart(Point(6.0,6.0),listOf(3.0,3.0,3.0),3))
-        mod.handleEvent(GameEvent.OnSurrend(3))
-    }
 
     @Test
     fun PlayerTest(){
