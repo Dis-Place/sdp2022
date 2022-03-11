041f930494a786c05c75cf496bce9cc867677f8b
larigaud <leo.larigauderie@epfl.ch>
2022-03-11 12:34:48 +0100

    added location property in demo map
    added basic location

--- app/src/main/java/com/github/blecoeur/bootcamp/DemoMapActivity.kt
+++ app/src/main/java/com/github/blecoeur/bootcamp/DemoMapActivity.kt
@@ -1,9 +1,15 @@
 package com.github.blecoeur.bootcamp
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.content.res.Configuration
+import android.location.Location
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.preference.PreferenceManager
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import org.osmdroid.config.Configuration.*
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory
 import org.osmdroid.util.GeoPoint
@@ -19,12 +25,41 @@ class DemoMapActivity : AppCompatActivity() {
     private val ZOOM = 16.0
     private val EPFL_POS = GeoPoint(46.52,6.57)
 
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private lateinit var userLocation: Location
+
     /**
      * @param savedInstanceState
      * creates a map view with a marker, centered on EPFL position
      */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+        } else {
+            fusedLocationClient.lastLocation
+                .addOnSuccessListener { location : Location? ->
+                    // Got last known location. In some rare situations this can be null.
+                    if(location != null){
+                        userLocation = location
+                    }
+                }
+        }
 
 
         getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this))
