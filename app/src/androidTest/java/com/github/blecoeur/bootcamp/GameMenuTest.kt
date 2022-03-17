c29b71f43a095be5007564a6d6d8e577b2367c89
Antoine Boyer <antoine.boyer@epfl.ch>
2022-03-17 18:24:42 +0100

    add activity

--- app/src/androidTest/java/com/github/blecoeur/bootcamp/GameMenuTest.kt
+++ app/src/androidTest/java/com/github/blecoeur/bootcamp/GameMenuTest.kt
@@ -19,7 +19,7 @@ import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
-class GameMenu {
+class GameMenuTest {
     @get:Rule
     val testRule = ActivityScenarioRule(MainActivity::class.java)
 
