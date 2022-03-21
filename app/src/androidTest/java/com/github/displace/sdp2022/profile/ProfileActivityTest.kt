de22893046f8343e1a519b94bd4d94e01bcd3f11
Blecoeur <56965256+Blecoeur@users.noreply.github.com>
2022-03-21 13:56:31 +0100

    Fixed import problems in ProfileActivityTest

--- app/src/androidTest/java/com/github/displace/sdp2022/profile/ProfileActivityTest.kt
+++ app/src/androidTest/java/com/github/displace/sdp2022/profile/ProfileActivityTest.kt
@@ -1,4 +1,4 @@
-package com.github.blecoeur.bootcamp.profile
+package com.github.displace.sdp2022.profile
 
 import android.content.Intent
 import android.view.View
@@ -14,12 +14,12 @@ import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import com.github.blecoeur.bootcamp.MainActivity
-import com.github.blecoeur.bootcamp.MyApplication
-import com.github.blecoeur.bootcamp.R
-import com.github.blecoeur.bootcamp.profile.friends.Friend
-import com.github.blecoeur.bootcamp.profile.friends.FriendViewHolder
-import com.github.blecoeur.bootcamp.profile.messages.MsgViewHolder
+import com.github.displace.sdp2022.MainActivity
+import com.github.displace.sdp2022.MyApplication
+import com.github.displace.sdp2022.R
+import com.github.displace.sdp2022.profile.friends.Friend
+import com.github.displace.sdp2022.profile.friends.FriendViewHolder
+import com.github.displace.sdp2022.profile.messages.MsgViewHolder
 import org.hamcrest.Matcher
 import org.junit.Rule
 import org.junit.Test
@@ -52,7 +52,7 @@ class ProfileActivityTest {
             Intent(ApplicationProvider.getApplicationContext(), ProfileActivity::class.java)
         val scenario = ActivityScenario.launch<ProfileActivity>(intent)
 
-        scenario.use { scenario ->
+        scenario.use {
             Espresso.onView(ViewMatchers.withId(R.id.inboxButton)).perform(click())
             Espresso.onView(ViewMatchers.withId(R.id.InboxScroll))
                 .check(ViewAssertions.matches(isDisplayed()))
@@ -69,7 +69,7 @@ class ProfileActivityTest {
             Intent(ApplicationProvider.getApplicationContext(), ProfileActivity::class.java)
         val scenario = ActivityScenario.launch<ProfileActivity>(intent)
 
-        scenario.use { scenario ->
+        scenario.use {
             Espresso.onView(ViewMatchers.withId(R.id.innerProfileButton)).perform(click())
             Espresso.onView(ViewMatchers.withId(R.id.ProfileScroll))
                 .check(ViewAssertions.matches(isDisplayed()))
@@ -86,7 +86,7 @@ class ProfileActivityTest {
             Intent(ApplicationProvider.getApplicationContext(), ProfileActivity::class.java)
         val scenario = ActivityScenario.launch<ProfileActivity>(intent)
 
-        scenario.use { scenario ->
+        scenario.use {
             Espresso.onView(ViewMatchers.withId(R.id.friendsButton)).perform(click())
             Espresso.onView(ViewMatchers.withId(R.id.FriendsScroll))
                 .check(ViewAssertions.matches(isDisplayed()))
@@ -103,7 +103,7 @@ class ProfileActivityTest {
             Intent(ApplicationProvider.getApplicationContext(), ProfileActivity::class.java)
         val scenario = ActivityScenario.launch<ProfileActivity>(intent)
 
-        scenario.use { scenario ->
+        scenario.use {
             Espresso.onView(ViewMatchers.withId(R.id.profileSettingsButton)).perform(click())
             Espresso.onView(ViewMatchers.withId(R.id.editProfile))
                 .check(ViewAssertions.matches(isDisplayed()))
@@ -145,7 +145,7 @@ class ProfileActivityTest {
         val intent =
             Intent(ApplicationProvider.getApplicationContext(), ProfileActivity::class.java)
         val scenario = ActivityScenario.launch<ProfileActivity>(intent)
-        scenario.use { scenario ->
+        scenario.use {
             Espresso.onView(ViewMatchers.withId(R.id.friendsButton)).perform(click())
             Espresso.onView(ViewMatchers.withId(R.id.recyclerFriend)).perform(
                 RecyclerViewActions.actionOnItemAtPosition<MsgViewHolder>(
@@ -172,7 +172,7 @@ class ProfileActivityTest {
             Intent(ApplicationProvider.getApplicationContext(), ProfileActivity::class.java)
         val scenario = ActivityScenario.launch<ProfileActivity>(intent)
 
-        scenario.use { scenario ->
+        scenario.use {
             Espresso.onView(ViewMatchers.withId(R.id.friendsButton)).perform(click())
             Espresso.onView(ViewMatchers.withId(R.id.recyclerFriend))
                 .perform(RecyclerViewActions.actionOnItemAtPosition<FriendViewHolder>(0, click()))
