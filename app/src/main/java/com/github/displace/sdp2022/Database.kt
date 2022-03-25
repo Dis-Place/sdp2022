c70e594d30316e36eef5c2f0fbc99bbfb2ba50fc
Abel Vexina Wilkinson <abel.vexinawilkinson@epfl.ch>
2022-03-25 10:13:15 +0100

    Database structure for project

--- app/src/main/java/com/github/displace/sdp2022/Database.kt
+++ app/src/main/java/com/github/displace/sdp2022/Database.kt
@@ -1,7 +1,11 @@
 package com.github.displace.sdp2022
 
+import com.google.android.gms.tasks.Task
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseReference
+
 interface Database {
-    fun instantiate(url: String) : Database
+    fun instantiate(url: String, debug : Boolean) : Database
 
     fun update(reference: String, key: String, obj: Any) : Any
 
@@ -10,4 +14,7 @@ interface Database {
     fun delete(reference: String, key: String)
 
     fun get(reference: String, key: String): Any?
+    fun referenceGet(reference: String, key: String): Task<DataSnapshot>
+    fun noCacheInstantiate(url: String, debug: Boolean): Database
+    fun getDbReference(path: String = ...: DatabaseReference
 }
--- app/src/main/java/com/github/displace/sdp2022/MyApplication.kt
+++ app/src/main/java/com/github/displace/sdp2022/MyApplication.kt
@@ -12,9 +12,25 @@ class MyApplication : Application() {
     private lateinit var db: MockDB
     private lateinit var activeUser: Friend
 
-    fun setDb(db: MockDB) {
-        this.db = db
+    private lateinit var rTdb : RealTimeDatabase
+    private lateinit var rTdbNoCache : RealTimeDatabase
+
+    fun setDb(db: RealTimeDatabase) {
+        this.rTdb = db
     }
+
+    fun setDbNonCache(db: RealTimeDatabase) {
+        this.rTdbNoCache = db
+    }
+
+    fun getDb() : RealTimeDatabase{
+        return rTdb
+    }
+
+    fun getDbNonCache() : RealTimeDatabase{
+        return rTdbNoCache
+    }
+
 
     fun getProfileDb(): ProfileDbConnection {
         return db
