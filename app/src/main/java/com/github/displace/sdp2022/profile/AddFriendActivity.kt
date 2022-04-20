f6087942687d99c59cedb35aa26942ce2a2bc9b9
Matthieu-Andre <matthieu.andre@epfl.ch>
2022-04-20 15:35:01 +0200

    modifications

--- app/src/main/java/com/github/displace/sdp2022/profile/AddFriendActivity.kt
+++ app/src/main/java/com/github/displace/sdp2022/profile/AddFriendActivity.kt
@@ -2,21 +2,35 @@ package com.github.displace.sdp2022.profile
 
 import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.github.displace.sdp2022.R
-import kotlinx.serialization.descriptors.PrimitiveKind
+import com.github.displace.sdp2022.users.PartialUser
+import com.google.firebase.database.*
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
+
+private const val TAG = "AddFriendActivity"
 
 class AddFriendActivity : AppCompatActivity() {
 
 
+    private lateinit var rootRef: DatabaseReference
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        Log.d(TAG, "Entering Activity")
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_friend)
+
+        rootRef = FirebaseDatabase.
+            getInstance("https://displace-dd51e-default-rtdb.europe-west1.firebasedatabase.app").
+            reference
     }
 
     fun sendFriendRequest(view: View) {
@@ -27,6 +41,8 @@ class AddFriendActivity : AppCompatActivity() {
 
         editText.text.clear()
         editText.hint = "Enter Another Friend"
+
+        checkIfUserExists("chad")
 
     }
 
@@ -39,7 +55,31 @@ class AddFriendActivity : AppCompatActivity() {
     }
 
     private fun checkIfUserExists(userName : String) : Boolean{
-        //TODO: implement
+        Log.d(TAG,"CHECK IF USER EXISTS")
+
+
+        val usersRef: DatabaseReference = rootRef.child("CompleteUsers")
+
+
+        val eventListener: ValueEventListener = object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot){
+                var partialUsers = mutableListOf<PartialUser>()
+                for (ds in dataSnapshot.children) {
+                    val partialUser = ds.child("CompleteUser").child("partialUser").child("uid").toString()
+                        Log.d(TAG, partialUser)
+//                    partialUsers.add(partialUser)
+                }
+
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {}
+        }
+
+
+        usersRef.addListenerForSingleValueEvent(eventListener)
+
+
+        return true
     }
 
     private fun sendInvite(){
