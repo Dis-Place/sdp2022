9bd7dc097a15cfa7246edc2dbf49cf4ba4e03651
Matthieu-Andre <matthieu.andre@epfl.ch>
2022-04-27 13:00:37 +0200

    send invites to server

--- app/src/main/java/com/github/displace/sdp2022/profile/AddFriendActivity.kt
+++ app/src/main/java/com/github/displace/sdp2022/profile/AddFriendActivity.kt
@@ -8,10 +8,11 @@ import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.github.displace.sdp2022.MyApplication
 import com.github.displace.sdp2022.R
+import com.github.displace.sdp2022.users.CompleteUser
 import com.github.displace.sdp2022.users.PartialUser
 import com.google.firebase.database.*
-import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 
 
@@ -21,7 +22,7 @@ class AddFriendActivity : AppCompatActivity() {
 
 
     private lateinit var rootRef: DatabaseReference
-
+    private lateinit var currentUser : PartialUser
 
     override fun onCreate(savedInstanceState: Bundle?) {
         Log.d(TAG, "Entering Activity")
@@ -31,6 +32,11 @@ class AddFriendActivity : AppCompatActivity() {
         rootRef = FirebaseDatabase.
             getInstance("https://displace-dd51e-default-rtdb.europe-west1.firebasedatabase.app").
             reference
+
+        val app = applicationContext as MyApplication
+        val activeUser = app.getActiveUser()
+
+        currentUser = activeUser?.getPartialUser() ?: PartialUser("dummy", "dummy")
     }
 
     fun sendFriendRequest(view: View) {
@@ -41,7 +47,7 @@ class AddFriendActivity : AppCompatActivity() {
 
 
 
-        checkIfUserExists("")
+        checkIfUserExists(editText.text.toString())
 
         editText.text.clear()
         editText.hint = "Enter Another Friend"
@@ -56,8 +62,8 @@ class AddFriendActivity : AppCompatActivity() {
         }
     }
 
-    private fun checkIfUserExists(userName : String) : Boolean{
-        Log.d(TAG,"CHECK IF USER $userName EXISTS")
+    private fun checkIfUserExists(target : String) : Boolean{
+        Log.d(TAG,"CHECK IF USER $target EXISTS")
 
 
         val usersRef: DatabaseReference = rootRef.child("CompleteUsers")
@@ -65,18 +71,15 @@ class AddFriendActivity : AppCompatActivity() {
 
         val eventListener: ValueEventListener = object : ValueEventListener {
             override fun onDataChange(dataSnapshot: DataSnapshot){
-                var partialUsers = mutableListOf<PartialUser>()
-                for (ds in dataSnapshot.children) {
-                    val uid = ds.child("CompleteUser").child("partialUser").child("uid").value.toString()
-                    val username = ds.child("CompleteUser").child("partialUser").child("username").value.toString()
-
-                    val partialUser = PartialUser(username,uid)
-                    Log.d(TAG, partialUser.toString())
-                    partialUsers.add(partialUser)
+                var partialUsers = getPartialUsers(dataSnapshot)
+                if( checkUserExists(partialUsers, target) ){
+                    val source = currentUser
+                    val target = getTargetUser(dataSnapshot, partialUsers, target)
+                    sendInvite(source, target)
                 }
 
-                val res = checkUserExists(partialUsers, userName)
-                Log.d(TAG, res.toString())
+
+
 
             }
 
@@ -90,11 +93,14 @@ class AddFriendActivity : AppCompatActivity() {
         return true
     }
 
-    private fun sendInvite(){
-        //TODO: implement
+    private fun sendInvite(source : PartialUser, target : PartialUser){
+        val inviteDbRef = rootRef.child("Invites")
+        val invite = Invite(source, target)
+        inviteDbRef.push().setValue(invite)
     }
 
     private fun checkUserExists(users : List<PartialUser>, target : String) : Boolean {
+        Log.d(TAG, "Searching if $target")
         for (user in users) {
             if( user.username == target ){
                 return true
@@ -102,4 +108,30 @@ class AddFriendActivity : AppCompatActivity() {
         }
         return false
     }
+
+    private fun getPartialUsers(dataSnapshot: DataSnapshot) : MutableList<PartialUser>{
+        var partialUsers = mutableListOf<PartialUser>()
+        for (ds in dataSnapshot.children) {
+            val uid = ds.child("CompleteUser").child("partialUser").child("uid").value.toString()
+            val username = ds.child("CompleteUser").child("partialUser").child("username").value.toString()
+
+            val partialUser = PartialUser(username,uid)
+//                    Log.d(TAG, partialUser.toString())
+            partialUsers.add(partialUser)
+        }
+        return partialUsers
+    }
+
+    private fun getTargetUser(dataSnapshot: DataSnapshot, users : List<PartialUser>, target : String) : PartialUser {
+        for (user in users) {
+            if( user.username == target ){
+                return user
+            }
+        }
+        return PartialUser("empty", "empty") // never gets executed as we check that the user exists before
+    }
+
+
+
+
 }
---
+++ app/src/main/java/com/github/displace/sdp2022/profile/Invite.kt
@@ -0,0 +1,18 @@
+package com.github.displace.sdp2022.profile
+
+import com.github.displace.sdp2022.users.PartialUser
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Invite(val source: PartialUser,val target : PartialUser){
+    override fun equals(other: Any?): Boolean {
+        val otherInvite = other as Invite?
+
+        return otherInvite?.source == source && otherInvite?.target == target
+    }
+
+    override fun hashCode(): Int {
+        return source.hashCode()
+    }
+}
