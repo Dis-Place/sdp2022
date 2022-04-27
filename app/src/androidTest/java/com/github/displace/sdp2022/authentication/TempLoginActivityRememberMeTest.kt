bd8e85b4b25cc8160ee5959c61b2b90bd1d75a8d
Franck <franck.khayat@epfl.ch>
2022-04-27 17:46:26 +0200

    Quick fix

--- app/src/androidTest/java/com/github/displace/sdp2022/authentication/TempLoginActivityRememberMeTest.kt
+++ app/src/androidTest/java/com/github/displace/sdp2022/authentication/TempLoginActivityRememberMeTest.kt
@@ -14,7 +14,7 @@ import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
-@RunWith(AndroidJUnit4::class)
+//@RunWith(AndroidJUnit4::class)
 class TempLoginActivityRememberMeTest {
    /* private var checkBoxState: Boolean = false
     private val context = InstrumentationRegistry.getInstrumentation().context
