581875da852ed722a52fc331a0e90eb3141ec9ad
Abel Vexina Wilkinson <abel.vexinawilkinson@epfl.ch>
2022-04-30 14:32:26 +0200

    QR code
    generates a QR code with the user information and is able to scan it (detecting if the QR code is incorrect)

--- app/src/androidTest/java/com/github/displace/sdp2022/MainMenuActivityTest.kt
+++ app/src/androidTest/java/com/github/displace/sdp2022/MainMenuActivityTest.kt
@@ -14,15 +14,12 @@ import androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent
 import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
-import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry.getInstrumentation
 import com.github.displace.sdp2022.news.NewsActivity
 import com.github.displace.sdp2022.profile.ProfileActivity
-import com.github.displace.sdp2022.profile.messages.MessageHandler
 import com.github.displace.sdp2022.users.CompleteUser
 import org.junit.Before
-import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
@@ -125,7 +122,7 @@ class MainMenuActivityTest {
         val scenario = ActivityScenario.launch<MainMenuActivity>(intent)
 
         scenario.use {
-            Espresso.onView(withId(R.id.dataBaseDemoButton)).perform(click())
+            Espresso.onView(withId(R.id.qrCodeDemo)).perform(click())
         }
 
         intended(hasComponent(UploadImageActivity::class.java.name))
---
+++ app/src/main/java/com/github/displace/sdp2022/profile/qrcode/QrCodeTemp.kt
@@ -0,0 +1,108 @@
+package com.github.displace.sdp2022.profile.qrcode
+
+import android.app.AlertDialog
+import android.content.DialogInterface
+import android.content.Intent
+import android.graphics.Bitmap
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.Group
+import com.budiyev.android.codescanner.AutoFocusMode
+import com.budiyev.android.codescanner.CodeScanner
+import com.budiyev.android.codescanner.CodeScannerView
+import com.budiyev.android.codescanner.DecodeCallback
+import com.budiyev.android.codescanner.ErrorCallback
+import com.budiyev.android.codescanner.ScanMode
+import com.github.displace.sdp2022.MyApplication
+import com.github.displace.sdp2022.R
+import com.github.displace.sdp2022.users.PartialUser
+import com.google.zxing.BarcodeFormat
+import com.journeyapps.barcodescanner.BarcodeEncoder
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+
+
+//the QR code does not need to receive any information for its creation
+class QrCodeTemp : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_qr_code_temp)
+
+    }
+
+
+    /**
+     * Generate a QR code and put its information in the corresponding field
+     * If we want to use this function somewhere else : place it in some Utils
+     * @argument field : an imageView
+     */
+    private fun generateQrCode(partialUser : PartialUser) : Bitmap? {
+        val width = 800
+        val height = 800
+        val qrCodeContent : String = Json.encodeToString(partialUser)
+
+        try{
+            val barcodeEncoder = BarcodeEncoder()
+            val bitmap = barcodeEncoder.encodeBitmap(qrCodeContent, BarcodeFormat.QR_CODE, width, height)
+            return bitmap
+        } catch(e : Exception) {
+            e.printStackTrace()
+        }
+        return null
+    }
+
+    /**
+     * Create the dialog view which will show the QR code
+     */
+    private fun createImagePopup(bmp : Bitmap){
+
+        val imageDialog: AlertDialog.Builder = AlertDialog.Builder(this)
+        val inflater = this.getSystemService(LAYOUT_INFLATER_SERVICE) as LayoutInflater
+
+        val layout: View = inflater.inflate(
+            R.layout.custom_image_dialog,
+            null
+        )
+        val image = layout.findViewById<View>(R.id.fullimage) as ImageView
+
+        image.setImageBitmap(bmp)
+
+        imageDialog.setView(layout)
+        imageDialog.setPositiveButton("OK",
+            DialogInterface.OnClickListener { dialog, _ -> dialog.dismiss() })
+
+        imageDialog.create()
+        imageDialog.show()
+    }
+
+    /**
+     * Show the QR code corresponding to the partial user - will be tasked of generating the QR code for now
+     */
+    fun showQrCode(view : View){
+        val app = applicationContext as MyApplication
+        val bmp = generateQrCode(app.getActiveUser()!!.getPartialUser())
+        if(bmp != null){
+            createImagePopup(bmp)
+        }
+
+    }
+
+
+    /**
+     * Show the scanner and use it : needs more research
+     */
+    fun useScanner(view : View){
+        val intent = Intent(this, QrCodeScannerActivity::class.java)
+        startActivity(intent)
+    }
+
+
+
+}
