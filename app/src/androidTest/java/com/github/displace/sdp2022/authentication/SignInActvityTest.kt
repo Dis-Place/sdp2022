01b6984f36e34d1d0c3a728158343e27bea85006
Blecoeur <56965256+Blecoeur@users.noreply.github.com>
2022-05-06 00:55:12 +0200

    Added tests

---
+++ app/src/androidTest/java/com/github/displace/sdp2022/authentication/SignInActvityTest.kt
@@ -0,0 +1,57 @@
+package com.github.displace.sdp2022.authentication
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.intent.Intents.*
+import androidx.test.espresso.intent.matcher.IntentMatchers.anyIntent
+import androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import com.github.displace.sdp2022.MainMenuActivity
+import com.github.displace.sdp2022.R
+import com.github.displace.sdp2022.users.CompleteUser
+import com.github.displace.sdp2022.users.OfflineUserFetcher
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class SignInActvityTest {
+
+    private lateinit var tempCompleteUser: CompleteUser
+    private val context = InstrumentationRegistry.getInstrumentation().context
+
+    @get:Rule
+    val activityScenarioRule = ActivityScenarioRule(SignInActivity::class.java)
+
+
+    @Test
+    fun guestModeButtonWorks() {
+        val tempCompleteUser = OfflineUserFetcher(context).getCompleteUser()
+
+        init()
+        onView(withId(R.id.signInActivityGuestModeButton)).perform(click())
+        Thread.sleep(2_000)
+        hasComponent(MainMenuActivity::class.java.name)
+        release()
+
+        OfflineUserFetcher(context).setCompleteUser(tempCompleteUser)
+    }
+
+    @Test
+    fun signInButtonWorks() {
+        val tempCompleteteUser = OfflineUserFetcher(context).getCompleteUser()
+
+        init()
+        onView(withId(R.id.signInActivitySignInButton)).perform(click())
+        Thread.sleep(2_000)
+        intended(anyIntent())
+        release()
+
+        OfflineUserFetcher(context).setCompleteUser(tempCompleteUser)
+    }
+
+
+}
---
+++ app/src/androidTest/java/com/github/displace/sdp2022/unitTest/MainActivityTest.kt
@@ -0,0 +1,66 @@
+import android.content.Context.MODE_PRIVATE
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.intent.Intents
+import androidx.test.espresso.intent.matcher.IntentMatchers
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import com.github.displace.sdp2022.MainActivity
+import com.github.displace.sdp2022.R
+import com.github.displace.sdp2022.authentication.SignInActivity
+import com.github.displace.sdp2022.users.CompleteUser
+import com.github.displace.sdp2022.users.OfflineUserFetcher
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class NewMainActivityTest {
+    private lateinit var copyCompleteUser: CompleteUser
+    private val context = InstrumentationRegistry.getInstrumentation().context
+
+    @get:Rule
+    val activityScenarioRule = ActivityScenarioRule(MainActivity::class.java)
+
+    @Before
+    fun setup() {
+        copyCompleteUser = OfflineUserFetcher(context).getCompleteUser()
+    }
+
+    @After
+    fun unSetup() {
+        OfflineUserFetcher(context).setCompleteUser(copyCompleteUser)
+    }
+
+    @Test
+    fun logInConnectionRemember() {
+        val bool =
+            context.getSharedPreferences("login", MODE_PRIVATE).getBoolean("remembered", false)
+        context.getSharedPreferences("login", MODE_PRIVATE).edit().putBoolean("remembered", true)
+            .apply()
+        Intents.init()
+        onView(withId(R.id.mainActivityLogInButton)).perform(click())
+        IntentMatchers.hasComponent(SignInActivity::class.java.name)
+        Intents.release()
+        context.getSharedPreferences("login", MODE_PRIVATE).edit().putBoolean("remembered", bool)
+            .apply()
+    }
+
+    @Test
+    fun logInConnectionNotRemember() {
+        val bool =
+            context.getSharedPreferences("login", MODE_PRIVATE).getBoolean("remembered", false)
+        context.getSharedPreferences("login", MODE_PRIVATE).edit().putBoolean("remembered", false)
+            .apply()
+        Intents.init()
+        onView(withId(R.id.mainActivityLogInButton)).perform(click())
+        IntentMatchers.hasComponent(SignInActivity::class.java.name)
+        Intents.release()
+        context.getSharedPreferences("login", MODE_PRIVATE).edit().putBoolean("remembered", bool)
+            .apply()
+    }
+}
