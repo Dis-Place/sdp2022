7e8b189a1de2d7d5e2e47802504d70171b2789f4
Franck <franck.khayat@epfl.ch>
2022-05-13 05:52:13 +0200

    Update of the News Activity UI to match the new design

--- app/src/main/java/com/github/displace/sdp2022/news/News.kt
+++ app/src/main/java/com/github/displace/sdp2022/news/News.kt
@@ -1,3 +1,5 @@
 package com.github.displace.sdp2022.news
 
-class News(val title: String, val description: String, val date: String)
+import android.graphics.Bitmap
+
+class News(val title: String, val description: String, val date: String, var image: Bitmap?)
