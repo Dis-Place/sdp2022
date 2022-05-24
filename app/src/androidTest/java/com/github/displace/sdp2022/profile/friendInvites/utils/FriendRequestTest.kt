51c5379ee7e940c56685e49910811ec71e467a7d
Matthieu-Andre <matthieu.andre@epfl.ch>
2022-05-24 19:28:34 +0200

    modified some tests, removed tests will be adapted to mockdb

--- app/src/androidTest/java/com/github/displace/sdp2022/profile/friendInvites/utils/FriendRequestTest.kt
+++ app/src/androidTest/java/com/github/displace/sdp2022/profile/friendInvites/utils/FriendRequestTest.kt
@@ -18,19 +18,7 @@ import org.mockito.Mockito.doNothing
 //https://semaphoreci.com/community/tutorials/stubbing-and-mocking-with-mockito-2-and-junit
 @RunWith(AndroidJUnit4::class)
 class FriendRequestTest {
-
-    @Test
-    fun sendFriendRequestTest(){
-        val partialUser = PartialUser("test","testUid")
-        val target = "dummy"
-        val mockContext = Mockito.mock(Context::class.java)
-        val mockDatabaseReference = Mockito.mock(DatabaseReference::class.java)
-        val mockInviteReference = Mockito.mock(DatabaseReference::class.java)
-        `when`(mockDatabaseReference.child("CompleteUsers")).thenReturn(mockInviteReference)
-        `when`(mockInviteReference.push()).thenReturn(mockInviteReference)
-       sendFriendRequest(mockContext, target, mockDatabaseReference, partialUser)
-
-    }
+    
 
     @Test
     fun checkUserExistsCheck(){
